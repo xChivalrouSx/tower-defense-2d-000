33f95b126961de0f9f0a79115a3232f4097037c8
Mert Çakar <mertcakar.es@gmail.com>
2023-12-06 23:30:24 +0300

    Enemy wawes and wawe countdown added.

---
+++ Assets/Scripts/WaweManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class WaweManager : MonoBehaviour
+{
+    [SerializeField] private Transform enemyPrefab;
+    [SerializeField] private TextMeshProUGUI countDownText;
+
+    private Transform spawnPoint;
+
+    private readonly float maxTimeBetweenWaves = 5f;
+    private float countdownBetweenWaves;
+
+    private int waveIndex = 0;
+
+    private void Start()
+    {
+        spawnPoint = PathManager.PathPoints[0];
+        countdownBetweenWaves = maxTimeBetweenWaves;
+        countDownText.text = countdownBetweenWaves.ToString("##");
+    }
+
+    private void Update()
+    {
+        if (countdownBetweenWaves <= 0f)
+        {
+            StartCoroutine(SpawnWawe());
+            countdownBetweenWaves = maxTimeBetweenWaves;
+        }
+        countdownBetweenWaves -= Time.deltaTime;
+
+        countDownText.text = countdownBetweenWaves.ToString("##"); // Mathf.Floor(countdown).ToString();
+    }
+
+    private IEnumerator SpawnWawe()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(.5f);
+
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
